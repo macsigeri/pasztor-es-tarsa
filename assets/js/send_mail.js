2521702c959decfe36c1ff6d0ff881c5eb8ada83
Gergely Macsi <gergely.macsi@bayzoltan.hu>
2024-03-04 11:42:28 +0100

    logo + email sending

---
+++ assets/js/send_mail.js
@@ -0,0 +1,21 @@
+(function(){
+
+    emailjs.init({
+        publicKey: 'DmlAEgiD2Wn6n9OpT',
+      });
+
+    const form = document.querySelector('#contact_form');
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+        emailjs.sendForm('service_h2u7133', 'template_khpn98e', this)
+            .then(() =>{
+                console.log('SUCCESS!');
+            }, (error)=>{
+                console.log('FAILED...', error);
+            });
+    });
+
+    
+})()
+
